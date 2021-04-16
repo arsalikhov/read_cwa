5dc6d51293889d158e72af2833fba04d7b784e37
arsalikhov <57370737+arsalikhov@users.noreply.github.com>
2021-04-16 13:42:15 -0400

    Update ax_to_edf.py

--- ax_to_edf.py
+++ ax_to_edf.py
@@ -11,13 +11,23 @@ def ax_to_edf(input_file_path, accelerometer_dir):
     axivity.CWAtoPandas(update= True)
     accelerometer_file_name = "accel.edf"
     temperature_file_name = "temp.edf"
+    gyro_file_name = "gyro.edf"
     device_location = "chest"
+
     physical_maxX = axivity.data['x'].max()
     physical_minX = axivity.data['x'].min()
     physical_maxY = axivity.data['y'].max()
     physical_minY = axivity.data['y'].min()
     physical_maxZ = axivity.data['z'].max()
     physical_minZ = axivity.data['z'].min()
+
+    Gphysical_maxX = axivity.data['gx'].max()
+    Gphysical_minX = axivity.data['gx'].min() 
+    Gphysical_maxY = axivity.data['gy'].max()
+    Gphysical_minY = axivity.data['gy'].min()
+    Gphysical_maxZ = axivity.data['gz'].max()
+    Gphysical_minZ = axivity.data['gz'].min()
+
 
 
 
@@ -57,6 +67,43 @@ def ax_to_edf(input_file_path, accelerometer_dir):
 
     accelerometer_file.writeSamples([axivity.data['x'], axivity.data['y'], axivity.data['z']])
     accelerometer_file.close()
+
+    gyroscope_full_path = os.path.join(accelerometer_dir, gyro_file_name)
+    gyroscope_file = pyedflib.EdfWriter(gyroscope_full_path, 3)
+    gyroscope_file.setHeader({"technician": "",
+                                "recording_additional": str(device_location),
+                                "patientname": "X",
+                                "patient_additional": "bs",
+                                "patientcode": "study_location_id",
+                                "equipment":str(axivity.file_info["uniqueSerialCode"]),
+                                "admincode": "",
+                                "gender": "male",
+                                "startdate": axivity.file_info["start"],
+                                "birthdate": datetime.datetime(1999, 8, 28)})
+
+    gyroscope_file.setSignalHeader(0, {"label": "gyroscope x", "dimension": "degree/s",
+                                            "sample_rate": axivity.file_info['frequency'],
+                                            "physical_max":Gphysical_maxX,
+                                            "physical_min": Gphysical_minX,
+                                            "digital_max": 32767, "digital_min": -32768,
+                                            "prefilter": "pre1", "transducer": "MEMS Accelerometer"})
+
+    gyroscope_file.setSignalHeader(1, {"label": "gyroscope y", "dimension": "degree/s",
+                                            "sample_rate": axivity.file_info['frequency'],
+                                            "physical_max": Gphysical_maxY,
+                                            "physical_min": Gphysical_minY,
+                                            "digital_max": 32767, "digital_min": -32768,
+                                            "prefilter": "pre1", "transducer": "MEMS Accelerometer"})
+
+    gyroscope_file.setSignalHeader(2, {"label": "gyroscope z", "dimension": "degree/s",
+                                            "sample_rate": axivity.file_info['frequency'],
+                                            "physical_max": Gphysical_maxZ,
+                                            "physical_min": Gphysical_minZ,
+                                            "digital_max": 32767, "digital_min": -32768,
+                                            "prefilter": "pre1", "transducer": "MEMS Accelerometer"})
+
+    gyroscope_file.writeSamples([axivity.data['gx'], axivity.data['gy'], axivity.data['gz']])
+    gyroscope_file.close()
 
     temperature_full_path = os.path.join(accelerometer_dir, temperature_file_name)
     temperature_file = pyedflib.EdfWriter(temperature_full_path, 1)
